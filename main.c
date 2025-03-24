ef7c061978041edec8c69e412b1923e9cd0f7fae
Nàt <82668500+4D4J@users.noreply.github.com>
2025-03-24 12:13:50 +0100

    New Update, player vs player mode works properly

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 #define MAX_NUMBER_JOUEURS 6
 #define MAX_SIZE_PSEUDO 50
+#define SCORE_GAGNANT 100
 
 int get_random_number(int min, int max) {
     return min + (rand() % (max - min + 1));
@@ -14,6 +14,12 @@ int get_random_number(int min, int max) {
 int main(void) {
     int nbJoueurs;
     char pseudos[MAX_NUMBER_JOUEURS][MAX_SIZE_PSEUDO];
+    int bank[MAX_NUMBER_JOUEURS] = {0};
+    int pointsTour;
+    int joueurActuel = 0;
+    int de;
+    char choix;
+    int partieTerminee = 0;
 
     srand(time(NULL));
 
@@ -32,7 +38,6 @@ int main(void) {
         printf("Entrez le pseudo du joueur %d : ", i + 1);
         fgets(pseudos[i], MAX_SIZE_PSEUDO, stdin);
 
-
         for (int j = 0; pseudos[i][j] != '\0'; j++) {
             if (pseudos[i][j] == '\n') {
                 pseudos[i][j] = '\0';
@@ -46,6 +51,54 @@ int main(void) {
         printf("Joueur %d : %s\n", i + 1, pseudos[i]);
     }
 
-    printf("%d",get_random_number(1,6));
+    printf("\nLe premier joueur a atteindre %d points gagne !\n\n", SCORE_GAGNANT);
+
+    while (!partieTerminee) {
+        pointsTour = 0;
+        int continuerTour = 1;
+
+        printf("\n--- Tour de %s ---\n", pseudos[joueurActuel]);
+        printf("Points en banque : %d\n", bank[joueurActuel]);
+
+        while (continuerTour) {
+            de = get_random_number(1, 6);
+            printf("Vous avez lancé un %d\n", de);
+
+            if (de == 1) {
+                printf("Dommage! Vous perdez les points de ce tour.\n");
+                pointsTour = 0;
+                continuerTour = 0;
+            } else {
+                pointsTour += de;
+                printf("Points de ce tour: %d\n", pointsTour);
+                printf("Total potentiel: %d\n", bank[joueurActuel] + pointsTour);
+
+                printf("Voulez-vous (R)elancer le de ou (S)auvegarder vos points ? ");
+                scanf(" %c", &choix);
+
+                if (choix == 's' || choix == 'S') {
+                    bank[joueurActuel] += pointsTour;
+                    printf("Points sauvegardes ! Vous avez maintenant %d points.\n", bank[joueurActuel]);
+                    continuerTour = 0;
+                }
+            }
+
+            if (bank[joueurActuel] >= SCORE_GAGNANT) {
+                printf("\n %s a gagne avec %d points! \n", pseudos[joueurActuel], bank[joueurActuel]);
+                partieTerminee = 1;
+                continuerTour = 0;
+            }
+        }
+
+        if (!partieTerminee) {
+            joueurActuel = (joueurActuel + 1) % nbJoueurs;
+        }
+    }
+
+    printf("\nScores finaux:\n");
+    for (int i = 0; i < nbJoueurs; i++) {
+        printf("%s: %d points\n", pseudos[i], bank[i]);
+    }
+
     return 0;
 }
